16d21f7d663c2d7b0f68a0c728b0487200e4dbaa
Han Byul <yahb9@naver.com>
2023-02-28 23:38:05 +0900

    Feat: Question component의 dropdown 생성
    - 선택하는 dropdown type에 따라 해당 위치로 dropdown이 이동
    - 선택 dropdownType을 useCallback으로 memoize, 중복 제거

---
+++ src/assets/svgs/index.tsx
@@ -0,0 +1,7 @@
+export { ReactComponent as CheckBoxIcon } from './checkBox.svg'
+export { ReactComponent as CheckBoxOutlineIcon } from './checkBoxOutline.svg'
+export { ReactComponent as CircleIcon } from './circle.svg'
+export { ReactComponent as FullCircleIcon } from './fullCircle.svg'
+export { ReactComponent as ParagraphIcon } from './paragraph.svg'
+export { ReactComponent as ShortTextIcon } from './shortText.svg'
+export { ReactComponent as DropdownIcon } from './dropdown.svg'
--- src/routes/Main/Question/index.tsx
+++ src/routes/Main/Question/index.tsx
@@ -1,9 +1,80 @@
+import { ChangeEvent, useCallback, useState } from 'react'
+
 import ContainerBox from 'components/ContainerBox'
 
+import { CheckBoxIcon, CircleIcon, DropdownIcon, ParagraphIcon, ShortTextIcon } from 'assets/svgs'
+import styles from './question.module.scss'
+
+const questionTypes = ['단답형', '장문형', '객관식 질문', '체크박스', '드롭다운']
+const dropdownIcons = (type: string) =>
+  ({
+    단답형: <ShortTextIcon className={styles.dropdownIcon} />,
+    장문형: <ParagraphIcon className={styles.dropdownIcon} />,
+    '객관식 질문': <CircleIcon className={styles.dropdownIcon} />,
+    체크박스: <CheckBoxIcon className={styles.dropdownIcon} />,
+    드롭다운: <DropdownIcon className={styles.dropdownIcon} />,
+  }[type])
+
 const Question = () => {
+  const [questionInput, setQuestionInput] = useState('제목 없는 질문')
+  const [isOpenDropdown, setIsOpenDropdown] = useState(false)
+  const [selectedDropdownType, setSelectedDropdownType] = useState({ type: '객관식 질문', order: 2 })
+
+  const handleQuestionInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setQuestionInput(e.currentTarget.value)
+  }
+
+  const handleOpenDropdownClick = () => {
+    setIsOpenDropdown(true)
+  }
+
+  const handleTypeDropdownClick = (type: string, order: number) => {
+    setSelectedDropdownType({ type, order })
+    setIsOpenDropdown(false)
+  }
+
+  const dropdownTypeItem = useCallback((type: string) => {
+    return (
+      <>
+        {dropdownIcons(type)}
+        <p>{type}</p>
+      </>
+    )
+  }, [])
+
   return (
     <ContainerBox boxType='question'>
-      <div>Question</div>
+      <div className={styles.questionHeader}>
+        <input
+          className={styles.question}
+          placeholder='질문'
+          value={questionInput}
+          onChange={handleQuestionInputChange}
+        />
+        {/* <QuestionTypeDropdown /> */}
+        <div className={styles.dropdownBox}>
+          <p className={styles.selectedType}>
+            <button type='button' onClick={handleOpenDropdownClick}>
+              {dropdownTypeItem(selectedDropdownType.type)}
+            </button>
+          </p>
+          {isOpenDropdown && (
+            <ul className={styles.dropdown} style={{ top: -(selectedDropdownType.order * 40) }}>
+              {questionTypes.map((type, index) => {
+                const typeKey = `typeKey-${index}`
+
+                return (
+                  <li key={typeKey}>
+                    <button type='button' onClick={() => handleTypeDropdownClick(type, index)}>
+                      {dropdownTypeItem(type)}
+                    </button>
+                  </li>
+                )
+              })}
+            </ul>
+          )}
+        </div>
+      </div>
     </ContainerBox>
   )
 }
